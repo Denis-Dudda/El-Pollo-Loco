904d9c7a54c20e0aa1239c405b20b5ffbf0395c9
Denis-Dudda <denis.d@hotmail.de>
2024-10-27 11:38:07 +0100

    change size enemies,character

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,10 @@
 class Character extends MovableObject {
+  x = 120;
+  y = 180;
+  height = 250;
+  width = 150;
+
+
   // der constructor wird immer zu erst ausgeführt wenn die klasse neu erstellt wird 
   constructor(){
     super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png')
