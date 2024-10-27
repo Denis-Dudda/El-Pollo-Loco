935a8762afec34cadd0edf1c5b5d01ed7182c3f4
Denis-Dudda <denis.d@hotmail.de>
2024-10-27 12:19:55 +0100

    add animate

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,10 +7,15 @@ class Cloud extends MovableObject {
 
     this.x = Math.random() * 500; // sorgt für zufälligen start punkt aber bei mindestens 200 pixel abstand
 
-    
+    this.animate();
   }
 
-
+  animate() {
+    setInterval(() => {
+      this.x -= 0.1;
+    },1000 / 60);
+    
+  }
 
 
 }
