bd621c4a73678a9e47d017e3bb2b6db98f39dcdb
Denis-Dudda <denis.d@hotmail.de>
2024-10-27 12:33:27 +0100

    loadImages for animation

--- models/character.class.js
+++ models/character.class.js
@@ -8,6 +8,18 @@ class Character extends MovableObject {
   // der constructor wird immer zu erst ausgeführt wenn die klasse neu erstellt wird 
   constructor(){
     super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png')
+    this.loadImages([
+      'img/2_character_pepe/1_idle/idle/I-1.png',
+      'img/2_character_pepe/1_idle/idle/I-2.png',
+      'img/2_character_pepe/1_idle/idle/I-3.png',
+      'img/2_character_pepe/1_idle/idle/I-4.png',
+      'img/2_character_pepe/1_idle/idle/I-5.png',
+      'img/2_character_pepe/1_idle/idle/I-6.png',
+      'img/2_character_pepe/1_idle/idle/I-7.png',
+      'img/2_character_pepe/1_idle/idle/I-8.png',
+      'img/2_character_pepe/1_idle/idle/I-9.png',
+      'img/2_character_pepe/1_idle/idle/I-10.png',
+    ]);
   }
 
 jump(){
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,22 @@ class MovableObject {
   height = 150;
   width = 100;
   img;
+  imageCache = {};
+
+ 
 
   loadImage(path){
     this.img = new Image(); // Image muss nicht neu definiert werden kommt vom js
     this.img.src = path; // der datei pfad den wir übergeben bekommen mit funktions aufruf
+  }
+
+  loadImages(arr) {
+    arr.forEach(path => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = path;      
+    });
+
   }
 
   moveRight(){
