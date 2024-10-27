20cabff1180a549ef8a3aabf05cad56f12e75efa
Denis-Dudda <denis.d@hotmail.de>
2024-10-27 08:50:43 +0100

    add constructor for character,chicken

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,8 @@
 class Character extends MovableObject {
+  // der constructor wird immer zu erst ausgeführt wenn die klasse neu erstellt wird 
+  constructor(){
+    super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png')
+  }
 
 jump(){
 
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,13 +3,17 @@ class MovableObject {
   y = 400;
   img;
 
+  loadImage(path){
+    this.img = new Image(); // Image muss nicht neu definiert werden kommt vom js
+    this.img.src = path; // der datei pfad den wir übergeben bekommen mit funktions aufruf
+  }
 
   moveRight(){
     console.log('Moving right');
   }
 
   moveLeft(){
-    
+
   }
 
 }
