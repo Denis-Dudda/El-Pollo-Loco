5db1b5ceddb2508464fdeb99728196dfdc8efdc0
Denis-Dudda <denis.d@hotmail.de>
2024-11-01 10:19:29 +0100

    test

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,7 +3,7 @@ class Chicken extends MovableObject {
   y = 355;
   height = 70;
   width = 60;
-  ENEMIES_WALKING = [
+  IMAGES_WALKING = [
     'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
     'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
     'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
@@ -12,7 +12,7 @@ class Chicken extends MovableObject {
 
   constructor(){
     super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
-    this.loadImages(this.ENEMIES_WALKING);
+    this.loadImages(this.IMAGES_WALKING);
     this.x = 200 + Math.random() * 500; // sorgt für zufälligen start punkt aber bei mindestens 200 pixel abstand
     
     this.animate();
@@ -23,8 +23,8 @@ class Chicken extends MovableObject {
   animate() {
     this.moveLeft();
     setInterval(() => {
-      let i = this.currentImage % this.ENEMIES_WALKING.length;
-      let path = this.ENEMIES_WALKING[i];
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
       this.img = this.imageCache[path];
       this.currentImage++;
     }, 200);
