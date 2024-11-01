22c3926a13625eddf38852bee11af90ec6d83993
Denis-Dudda <denis.d@hotmail.de>
2024-11-01 12:46:12 +0100

    add bottles

--- models/bottles.class.js
+++ models/bottles.class.js
@@ -3,14 +3,14 @@ class Bottle extends MovableObject {
     width = 80;
     height = 80;
 
-  IMAGES_WALKING = [
+  BOTTLE_MOVING = [
     'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
     'img/6_salsa_bottle/2_salsa_bottle_on_ground.png',
   ];
 
   constructor(){
     super().loadImage('img/6_salsa_bottle/1_salsa_bottle_on_ground.png')
-    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.BOTTLE_MOVING);
     this.x = 300;
     this.y = 350;
     this.animate();
@@ -19,7 +19,7 @@ class Bottle extends MovableObject {
   animate() {
     
     setInterval(() => {
-      this.playAnimation(this.IMAGES_WALKING);
+      this.playAnimation(this.BOTTLE_MOVING);
     }, 400);
   }
 
--- models/coins.class.js
+++ models/coins.class.js
@@ -1,13 +1,13 @@
 class Coin extends MovableObject {
 
-  IMAGES_WALKING = [
+  COIN_MOVING = [
     'img/8_coin/coin_1.png',
     'img/8_coin/coin_2.png',
   ];
 
   constructor(){
     super().loadImage('img/8_coin/coin_1.png')
-    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.COIN_MOVING);
     this.x = 300;
     this.y = 50;
     this.animate();
@@ -16,7 +16,7 @@ class Coin extends MovableObject {
   animate() {
     
     setInterval(() => {
-      this.playAnimation(this.IMAGES_WALKING);
+      this.playAnimation(this.COIN_MOVING);
     }, 400);
   }
 
