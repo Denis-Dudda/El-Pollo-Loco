ed3c2fc1a5faa6fab40382b78247ec0b16ffbeb2
Denis-Dudda <denis.d@hotmail.de>
2024-11-01 12:59:29 +0100

    add jump animation

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,19 @@ class Character extends MovableObject {
     'img/2_character_pepe/2_walk/W-25.png',
     'img/2_character_pepe/2_walk/W-26.png',
   ];
+
+  IMAGES_JUMPING = [
+    'img/2_character_pepe/3_jump/J-31.png',
+    'img/2_character_pepe/3_jump/J-32.png',
+    'img/2_character_pepe/3_jump/J-33.png',
+    'img/2_character_pepe/3_jump/J-34.png',
+    'img/2_character_pepe/3_jump/J-35.png',
+    'img/2_character_pepe/3_jump/J-36.png',
+    'img/2_character_pepe/3_jump/J-37.png',
+    'img/2_character_pepe/3_jump/J-38.png',
+    'img/2_character_pepe/3_jump/J-39.png',
+    'img/2_character_pepe/3_jump/J-40.png',
+  ]
   world;
   waking_sound = new Audio('audio/walking.mp3')
 
@@ -20,6 +33,7 @@ class Character extends MovableObject {
   constructor(){
     super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png')
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.applyGravity();
     this.animate();
   }
@@ -46,6 +60,10 @@ class Character extends MovableObject {
 
     // animation für sie bilder die angezeigt werden z.b das gehen 
     setInterval(() => {
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING);
+      }
+
       if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
         this.playAnimation(this.IMAGES_WALKING);
       }
