d3cd6da94b04ad3b849735d425954e5dbd6e6d70
Denis-Dudda <denis.d@hotmail.de>
2024-11-30 09:44:24 +0100

    js doc

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,105 +1,202 @@
-class MovableObject extends DrawableObject{
+/**
+ * Represents an object that can move within the game world.
+ * This class includes basic functionality such as movement, gravity, collision detection,
+ * energy management, and jumping. It is extended by other objects that need movement and interaction.
+ */
+class MovableObject extends DrawableObject {
 
+  /**
+   * The horizontal speed of the object.
+   * @type {number}
+   */
   speed = 0.1;
+
+  /**
+   * A flag indicating whether the object is facing the opposite direction.
+   * @type {boolean}
+   */
   otherDirection = false;
+
+  /**
+   * The vertical speed of the object (used for gravity and jumping).
+   * @type {number}
+   */
   speedY = 0;
+
+  /**
+   * The acceleration due to gravity.
+   * @type {number}
+   */
   acceleration = 2.5;
+
+  /**
+   * The energy of the object (typically represents health or hit points).
+   * @type {number}
+   */
   energy = 100;
+
+  /**
+   * The energy associated with collecting coins.
+   * @type {number}
+   */
   coinEnergy = 0;
+
+  /**
+   * The energy associated with collecting bottles.
+   * @type {number}
+   */
   bottleEnergy = 0;
+
+  /**
+   * The timestamp of the last hit the object received.
+   * @type {number}
+   */
   lastHit = 0;
-    
-  applyGravity(){
+
+  /**
+   * Applies gravity to the object, adjusting its vertical position and speed.
+   * @memberof MovableObject
+   */
+  applyGravity() {
     setInterval(() => {
       if (this.isAboveGround() || this.speedY > 0) {
         this.y -= this.speedY;
         this.speedY -= this.acceleration;        
       }    
-    }, 1000 / 25);
+    }, 1000 / 25); // Apply gravity in intervals of 1/25th of a second.
   }
 
+  /**
+   * Checks if the object is above the ground.
+   * For throwable objects, this checks if it is falling (y < 350).
+   * @returns {boolean} True if the object is above the ground, false otherwise.
+   */
   isAboveGround() {
-    if (this instanceof ThrowableObject) { // flasche fällt auf den boden
-      return this.y < 350;
-    }else {
-    return this.y < 180;
-     }
+    if (this instanceof ThrowableObject) { // For throwable objects like bottles
+      return this.y < 350; // Bottle is falling if its y position is less than 350
+    } else {
+      return this.y < 180; // Default value for other objects
+    }
   }
 
-
-  isColliding (mo) {
+  /**
+   * Checks if the object is colliding with another object.
+   * @param {MovableObject} mo The object to check collision with.
+   * @returns {boolean} True if there is a collision, false otherwise.
+   */
+  isColliding(mo) {
     return (
       this.x + this.offsetX + this.width - this.offsetWidth > mo.x + mo.offsetX &&
       this.y + this.offsetY + this.height - this.offsetHeight > mo.y + mo.offsetY &&
       this.x + this.offsetX < mo.x + mo.offsetX + mo.width - mo.offsetWidth &&
       this.y + this.offsetY < mo.y + mo.offsetY + mo.height - mo.offsetHeight
-  );
+    );
   }
 
+  /**
+   * Checks if the object is "jumping on" an enemy (colliding from above).
+   * @param {MovableObject} mo The enemy object to check against.
+   * @returns {boolean} True if the object is jumping on the enemy, false otherwise.
+   */
   jumpOnEnemy(mo) {
-    let xTolerance = 0; // Toleranz in Pixeln für die horizontale Kollision
-    let yTolerance = 80; // Toleranz in Pixeln für die vertikale Kollision
+    let xTolerance = 0; // Horizontal tolerance for collision detection (in pixels)
+    let yTolerance = 80; // Vertical tolerance for collision detection (in pixels)
 
     return (
-        this.x + this.offsetX + this.width - this.offsetWidth - xTolerance > mo.x + mo.offsetX &&
-        this.x + this.offsetX + xTolerance < mo.x + mo.offsetX + mo.width - mo.offsetWidth &&
-        this.y + this.offsetY + this.height - this.offsetHeight - yTolerance > mo.y + mo.offsetY &&
-        this.y + this.offsetY + yTolerance < mo.y + mo.offsetY + mo.height - mo.offsetHeight &&
-        this.speedY < 0 // Stellt sicher, dass der Charakter nach unten fällt
+      this.x + this.offsetX + this.width - this.offsetWidth - xTolerance > mo.x + mo.offsetX &&
+      this.x + this.offsetX + xTolerance < mo.x + mo.offsetX + mo.width - mo.offsetWidth &&
+      this.y + this.offsetY + this.height - this.offsetHeight - yTolerance > mo.y + mo.offsetY &&
+      this.y + this.offsetY + yTolerance < mo.y + mo.offsetY + mo.height - mo.offsetHeight &&
+      this.speedY < 0 // Ensures that the character is falling (i.e., jumping on the enemy)
     );
-}
+  }
 
+  /**
+   * Sets the collision offsets for the object to adjust the area checked for collisions.
+   * @param {number} offsetX The horizontal offset for the collision area.
+   * @param {number} offsetY The vertical offset for the collision area.
+   * @param {number} offsetWidth The horizontal width offset for the collision area.
+   * @param {number} offsetHeight The vertical height offset for the collision area.
+   */
   setCollisionOffsets(offsetX, offsetY, offsetWidth, offsetHeight) {
     this.offsetX = offsetX;
     this.offsetY = offsetY;
     this.offsetWidth = offsetWidth;
     this.offsetHeight = offsetHeight;
-}
+  }
 
-  catchCoin(){
+  /**
+   * Increases the coin energy of the object when a coin is collected.
+   */
+  catchCoin() {
     this.coinEnergy += 10;
     if (this.coinEnergy > 100) {
-      this.coinEnergy = 100;
+      this.coinEnergy = 100; // Cap the coin energy at 100
     }
   }
 
-  catchBottle(){
+  /**
+   * Increases the bottle energy of the object when a bottle is collected.
+   */
+  catchBottle() {
     this.bottleEnergy += 20;
     if (this.bottleEnergy > 100) {
-      this.bottleEnergy = 100;
+      this.bottleEnergy = 100; // Cap the bottle energy at 100
     }
   }
 
-  hit(){
+  /**
+   * Reduces the object's energy by 20 when it is hit.
+   * @memberof MovableObject
+   */
+  hit() {
     this.energy -= 20;
     if (this.energy < 0) {
-        this.energy = 0;
+      this.energy = 0; // Ensure energy does not drop below 0
     } else {
-      this.lastHit = new Date().getTime();  // zeit vom treffer
+      this.lastHit = new Date().getTime();  // Store the timestamp of when the hit occurred
     }
   }
 
-  isHurt(){
-    let timepassed = new Date().getTime() - this.lastHit; //difference in ms
-    timepassed = timepassed / 1000; //difference in s
-    return timepassed < 0.5; // wie lange die animation gezeigt wird
+  /**
+   * Checks if the object is still hurt (i.e., it was recently hit).
+   * @returns {boolean} True if the object is hurt, false otherwise.
+   */
+  isHurt() {
+    let timePassed = new Date().getTime() - this.lastHit; // Calculate the time since the last hit
+    timePassed = timePassed / 1000; // Convert time from milliseconds to seconds
+    return timePassed < 0.5; // Return true if less than 0.5 seconds have passed since the hit
   }
 
-  isDead(){
-    return this.energy == 0;
+  /**
+   * Checks if the object is dead (i.e., its energy is 0).
+   * @returns {boolean} True if the object is dead, false otherwise.
+   */
+  isDead() {
+    return this.energy === 0;
   }
 
-  playAnimation(images){
+  /**
+   * Plays an animation by cycling through the images.
+   * @param {string[]} images The array of image paths to be used in the animation.
+   */
+  playAnimation(images) {
     let i = this.currentImage % images.length;
     let path = images[i];
-    this.img = this.imageCache[path];
+    this.img = this.imageCache[path]; // Set the image from the cache based on the current image index.
     this.currentImage++;
   }
-  
-  moveRight(){
+
+  /**
+   * Moves the object to the right by its speed.
+   */
+  moveRight() {
     this.x += this.speed;
   }
 
+  /**
+   * Moves the object to the left by its speed.
+   */
   moveLeft() {
     this.x -= this.speed;
   }
--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,5 +1,14 @@
+/**
+ * Represents a throwable object, such as a bottle, in the game.
+ * This class extends the MovableObject class and is responsible for managing the bottle's throw,
+ * its animations, and its collision detection.
+ */
 class ThrowableObject extends MovableObject {
 
+  /**
+   * Images representing the bottle's rotation during a throw.
+   * @type {string[]}
+   */
   IMAGES_BOTTLETHROW = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
@@ -7,6 +16,10 @@ class ThrowableObject extends MovableObject {
     "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
   ];
 
+  /**
+   * Images representing the splash effect when the bottle hits the ground.
+   * @type {string[]}
+   */
   IMAGES_SPLASH = [
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
@@ -16,43 +29,57 @@ class ThrowableObject extends MovableObject {
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
 
+  /**
+   * Creates an instance of the ThrowableObject (bottle) and initializes its properties.
+   * @param {number} x - The initial x position of the bottle.
+   * @param {number} y - The initial y position of the bottle.
+   */
   constructor(x, y) {
-    super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png')
+    super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
     this.loadImages(this.IMAGES_BOTTLETHROW);
     this.loadImages(this.IMAGES_SPLASH);
-    this.setCollisionOffsets(0, 0, 0, 0);
+    this.setCollisionOffsets(0, 0, 0, 0); // Set the collision area offsets for the object.
     this.x = x;
     this.y = y;
     this.height = 60;
     this.width = 50;
-    this.world = world;
-    this.throw();
+    this.world = world; // Assign the world reference to the object.
+    this.throw(); // Start the throwing animation and physics.
   }
 
-  throw(){   
-      this.speedY = 30;
-      this.applyGravity();
-      if (this.world.character.otherDirection){
-        setInterval(() => {
-          this.x -= 10;
-        }, 40);
-      }else{
-        setInterval(() => {
-          this.x += 10;
-        }, 40);
-      }
+  /**
+   * Initiates the throwing action for the bottle, applying gravity and moving the bottle
+   * horizontally based on the direction the character is facing.
+   */
+  throw() {
+    this.speedY = 30; // Vertical speed of the throw.
+    this.applyGravity(); // Apply gravity to the bottle's movement.
+
+    // Move the bottle horizontally based on the character's direction.
+    if (this.world.character.otherDirection) {
       setInterval(() => {
-        if (this.world.character.otherDirection && this.y > 350) {
-          this.playAnimation(this.IMAGES_SPLASH);
-          this.x += 10;
-        }else
-        if (!this.world.character.otherDirection && this.y > 350) {
-          this.playAnimation(this.IMAGES_SPLASH);
-          this.x -= 10;
-        }
-        else {
-          this.playAnimation(this.IMAGES_BOTTLETHROW);
-        }        
-     }, 40);
+        this.x -= 10; // Move left if the character is facing left.
+      }, 40);
+    } else {
+      setInterval(() => {
+        this.x += 10; // Move right if the character is facing right.
+      }, 40);
     }
+
+    // Change the animation of the bottle based on its vertical position.
+    setInterval(() => {
+      if (this.world.character.otherDirection && this.y > 350) {
+        // Play splash animation if the bottle hits the ground and the character is facing left.
+        this.playAnimation(this.IMAGES_SPLASH);
+        this.x += 10;
+      } else if (!this.world.character.otherDirection && this.y > 350) {
+        // Play splash animation if the bottle hits the ground and the character is facing right.
+        this.playAnimation(this.IMAGES_SPLASH);
+        this.x -= 10;
+      } else {
+        // Play throwing animation while the bottle is in the air.
+        this.playAnimation(this.IMAGES_BOTTLETHROW);
+      }
+    }, 40); // Update every 40ms for smooth animation.
+  }
 }
